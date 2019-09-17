9e0e0b03f2cb3da021df68dcab614bf80661c792
HardikLohogaonkar <33368156+HardikLohogaonkar@users.noreply.github.com>
2019-09-17 15:11:58 +0530

    Update BiometricCallbackV28.java

--- fingerprintauth/src/main/java/com/choicetech/fingerprintauth/callback/BiometricCallbackV28.java
+++ fingerprintauth/src/main/java/com/choicetech/fingerprintauth/callback/BiometricCallbackV28.java
@@ -19,6 +19,7 @@ public class BiometricCallbackV28 extends BiometricPrompt.AuthenticationCallback
     @Override
     public void onAuthenticationSucceeded(BiometricPrompt.AuthenticationResult result) {
         super.onAuthenticationSucceeded(result);
+        dismissDialog()
         biometricCallback.onAuthenticationSuccessful();
     }
 
